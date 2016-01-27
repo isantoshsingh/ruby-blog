9d82888ec032fc12313209ff11fcb4a4613dc5f3
Santosh Singh <santosh@antstack.com>
2016-01-27 19:52:06 +0530

    bootstrap added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,25 @@
 Rails.application.routes.draw do
+
+  get 'articles/index'
+
+  get 'articles/new'
+
+  get 'articles/create'
+
+  get 'articles/edit'
+
+  get 'articles/show'
+
+  resources :articles
+  
+  get 'welcome/hello'
+  get 'welcome/about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcome#hello'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
