8ac7e48e0fca22536bfd0123fb4d31f49ea7666a
Santosh Singh <santosh@antstack.com>
2016-02-15 20:42:06 +0530

    Blog: edit/view functionality added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,9 +2,11 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    @articles = Article.all
   end
 
   def new
+    @article = Article.new
   end
 
   def create
@@ -20,8 +22,18 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  
   def show
     @article = Article.find(params[:id])
   end
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Blog
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    #This will use your sever's timezone on the database
+    config.time_zone = 'Asia/Kolkata'
   end
 end
