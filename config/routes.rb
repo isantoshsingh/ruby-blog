f461683863ac05f02ac648b38a3ba2cd81219e95
Santosh Singh <santosh@antstack.com>
2016-03-01 15:32:52 +0530

    removed unnecessary controller

--- config/routes.rb
+++ config/routes.rb
@@ -20,15 +20,12 @@ Rails.application.routes.draw do
   get 'articles/show'
 
   resources :articles
-  
-  get 'welcome/hello'
-  get 'welcome/about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+   root 'articles#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
